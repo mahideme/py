7df887dc0a3d3cd2c66dea186b914d861a2fb009
mahideme <mahletdemeke95@gmail.com>
2024-04-23 13:58:13 +0300

    gtst1

---
+++ gtstpy.py
@@ -0,0 +1,8 @@
+gtst="Day 7"
+print(f"Hello today is our {gtst} course")
+num=[0,2,4]
+for i, num in enumerate(num):
+    print(f"The {i+1}st, Even number is : {num} ")
+fruits={"apple":10, "banana":15, "pineapple":20}
+choice= input("Enter your choise: ")
+print(f"The value of {choice} is: {fruits[choice]}")
